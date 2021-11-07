a9b9d7503eeff25fb5629f98c660dab1c9a69264
Davit <david.sokhakyan@gmail.com>
2021-11-07 09:54:47 -0800

    get address transaction final test example

--- getAddressTransactions.js
+++ getAddressTransactions.js
@@ -8,11 +8,11 @@ let wallet = null;
 // VTa6MGEipZNAsKyGsgZuqdx1VirHA2E44P
 let account = null;
 const options = {};
-wallet = new Wallet({...options, network: 'testnet', address: 'VLssV73exU8smfjHBTBizfV5SjZqHft1NL',
+wallet = new Wallet({...options, network: 'testnet', address: 'VTa6MGEipZNAsKyGsgZuqdx1VirHA2E44P',
     transport: {dapiAddresses: ['127.0.0.1:2501:8080']}});
 wallet.getAccount().then(async (acc) => {
     account = acc;
-    const response = await wallet.getTransactions("VLssV73exU8smfjHBTBizfV5SjZqHft1NL");
+    const response = await wallet.getTransactions("VTa6MGEipZNAsKyGsgZuqdx1VirHA2E44P", 2, 3);
     console.log(response);
 }).finally(() => {
     wallet.disconnect();
